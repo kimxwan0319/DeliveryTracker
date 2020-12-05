7dbf2ea4d82aeee0fe58000900ef26ac0912d486
kimxwan0319 <kimxwan0319@naver.com>
2020-12-06 01:18:54 +0900

    Add File

---
+++ DeliveryTracker/DeliveryTracker/PostInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostInfoViewController.swift
+//  DeliveryTracker
+//
+//  Created by 김수완 on 2020/12/06.
+//
+
+import UIKit
+
+class PostInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
