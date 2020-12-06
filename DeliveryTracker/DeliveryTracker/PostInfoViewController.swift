c58ab3ca891d7d626c4b1fb8b21253fd64f0e71f
kimxwan0319 <kimxwan0319@naver.com>
2020-12-06 09:16:55 +0900

    publishing progreesesCell

--- DeliveryTracker/DeliveryTracker/PostInfoViewController.swift
+++ DeliveryTracker/DeliveryTracker/PostInfoViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class PostInfoViewController: UIViewController {
 
+    @IBOutlet weak var stateLable: UILabel!
+    @IBOutlet weak var carrierLable: UILabel!
+    @IBOutlet weak var postNumberLable: UILabel!
+    @IBOutlet weak var fromLable: UILabel!
+    @IBOutlet weak var toLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PostInfoViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! progressesCell
+        
+        return cell
+    }
+    
+    
+}
 
+class progressesCell: UITableViewCell {
+    @IBOutlet weak var dateLable: UILabel!
+    @IBOutlet weak var timeLable: UILabel!
+    @IBOutlet weak var lineImageView: UIImageView!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var discriptionLable: UILabel!
 }
